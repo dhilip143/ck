fb368ab9ac0a25012242aebfc7a5bdd72e6e9feb
dhilip143 <kdhilip146@gmail.com>
2025-10-03 11:59:19 +0530

    fo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Form from "/src/homecomponents/Form.jsx";
 import Grow from "/src/homecomponents/Grow.jsx";
 import Insidecavin from "../homecomponents/Insidecavin";
 import Purpose from "../homecomponents/purpose";
-import Small from "src/homecomponents/small.jsx";
+import Small from "/src/homecomponents/small.jsx";
 
 import Landing from "/src/homecomponents/Landing.jsx";
 import Years from "/src/homecomponents/years.jsx"
